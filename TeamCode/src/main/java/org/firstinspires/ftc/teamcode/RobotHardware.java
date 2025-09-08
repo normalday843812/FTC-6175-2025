9eb9015666e71929a2375fb1cc8cbd7e67823cd4
normalday843812 <kaistewart2019@gmail.com>
2025-09-07 14:43:30 -1000

    Added mecanum and initial teleop Added ftclib as a dependency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -1,17 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class RobotHardware {
-    private LinearOpMode inputOpMode = null;
+    private final LinearOpMode inputOpMode;
     // Constructor for OpMode
     public RobotHardware(LinearOpMode opMode) { inputOpMode = opMode; }
 
+    // Pinpoint
+    GoBildaPinpointDriver pinpoint;
+    public static double pinpointXOffset = -84.0; // TODO: replace with our actual values
+    public static double pinpointYOffset = -168.0; // TODO: replace with our actual values
+
+    // Motors
     private DcMotorEx frontRightMotor, frontLeftMotor, backRightMotor, backLeftMotor;
-    public void Init() {
+    public void initHardware() {
         // Motors
         frontRightMotor = inputOpMode.hardwareMap.get(DcMotorEx.class, "front_right_drive");
         frontLeftMotor = inputOpMode.hardwareMap.get(DcMotorEx.class, "front_left_drive");
@@ -27,10 +36,33 @@ public class RobotHardware {
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set motor encoders
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Pinpoint
+        pinpoint = inputOpMode.hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        configurePinpoint();
     }
 
+    public void configurePinpoint(){
+        pinpoint.setOffsets(pinpointXOffset, pinpointYOffset, DistanceUnit.MM);
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD); // TODO: Replace with our robot's configuration
+        pinpoint.resetPosAndIMU();
+    }
+
+
+    // Getters
     public DcMotorEx getFrontRight() { return frontRightMotor; }
     public DcMotorEx getFrontLeft()  { return frontLeftMotor; }
     public DcMotorEx getBackRight()  { return backRightMotor; }
     public DcMotorEx getBackLeft()   { return backLeftMotor; }
+
+    public GoBildaPinpointDriver getPinpoint() { return pinpoint; }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localisation/StateEstimator.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.localisation;
+
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
+
+// TODO: Add localisation via apriltag detection implementation from vision package
+public class StateEstimator {
+    // Pinpoint
+    private final GoBildaPinpointDriver pinpoint;
+
+    // Constructor
+    public StateEstimator(GoBildaPinpointDriver pinpoint) {
+        this.pinpoint = pinpoint;
+    }
+
+    // Must call once per loop for updated data
+    public void update() {
+        pinpoint.update();
+    }
+
+    // Returns field pose in meters and radians
+    public Pose2D getPose() {
+        double x = pinpoint.getPosX(DistanceUnit.METER);
+        double y = pinpoint.getPosY(DistanceUnit.METER);
+        double h = pinpoint.getHeading(AngleUnit.RADIANS);
+        return new Pose2D(DistanceUnit.METER, x, y, AngleUnit.RADIANS, h);
+    }
+
+    public double getHeading() {
+        return pinpoint.getHeading(AngleUnit.RADIANS);
+    }
+
+    public ChassisSpeeds getChassisSpeedsField() {
+        double vx = pinpoint.getVelX(DistanceUnit.METER);
+        double vy = pinpoint.getVelY(DistanceUnit.METER);
+        double omega = pinpoint.getHeadingVelocity(UnnormalizedAngleUnit.RADIANS);
+        return new ChassisSpeeds(vx, vy, omega);
+    }
+
+    public ChassisSpeeds getChassisSpeedsRobot() {
+        double vxF = pinpoint.getVelX(DistanceUnit.METER);
+        double vyF = pinpoint.getVelY(DistanceUnit.METER);
+        double h = pinpoint.getHeading(AngleUnit.RADIANS);
+        double cos = Math.cos(h), sin = Math.sin(h);
+
+        double vxR = vxF * cos + vyF * sin;
+        double vyR = -vxF * sin + vyF * cos;
+        double omega = pinpoint.getHeadingVelocity(UnnormalizedAngleUnit.RADIANS);
+
+        return new ChassisSpeeds(vxR, vyR, omega);
+    }
+}
