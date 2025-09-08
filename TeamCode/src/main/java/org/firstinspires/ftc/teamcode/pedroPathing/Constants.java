57f9ab98d66c15b1c66b2ddd8227f5e3508f1581
normalday843812 <kaistewart2019@gmail.com>
2025-09-07 16:31:28 -1000

    Implement new localiser

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -2,27 +2,38 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
-import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.localization.Localizer;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static final FollowerConstants followerConstants = new FollowerConstants();
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static final PathConstraints pathConstraints = new PathConstraints(
+            0.99,
+            100,
+            1,
+            1
+    );
 
-    public static Follower createFollower(HardwareMap hardwareMap) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
-                .pinpointLocalizer(localizerConstants)
-                .pathConstraints(pathConstraints)
-                .build();
-    }
+    public static final String WEBCAM_NAME = "Webcam 1";
+    public static final Position CAMERA_POS =
+            new Position(DistanceUnit.INCH, -2.0, 6.0, 8.0, 0);
 
-    public static PinpointConstants localizerConstants = new PinpointConstants()
+    public static final YawPitchRollAngles CAMERA_ORIENT =
+            new YawPitchRollAngles(AngleUnit.RADIANS, 0, -Math.PI / 2, 0, 0);
+
+    public static final Pose START_POSE = new Pose(0, 0, 0);
+
+    public static final PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(-5)
             .strafePodX(0.5)
             .distanceUnit(DistanceUnit.INCH)
@@ -30,4 +41,27 @@ public class Constants {
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        Localizer fusedLocalizer = new WebcamPinpointLocaliser(
+                hardwareMap,
+                localizerConstants,
+                START_POSE,
+                WEBCAM_NAME,
+                CAMERA_POS,
+                CAMERA_ORIENT
+        );
+
+        PedroMecanumDrivetrain drivetrain = new PedroMecanumDrivetrain(
+                hardwareMap,
+                "front_right_drive", "front_left_drive", "back_right_drive", "back_left_drive"
+        );
+
+        return new Follower(
+                followerConstants,
+                fusedLocalizer,
+                drivetrain,
+                pathConstraints
+        );
+    }
 }
