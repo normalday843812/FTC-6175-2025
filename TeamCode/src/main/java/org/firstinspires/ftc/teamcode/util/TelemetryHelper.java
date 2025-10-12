ca53bd16126c8635f5d07bf2bcc3a91ca95b47f7
normalday843812 <kaistewart2019@gmail.com>
2025-10-12 12:45:51 -1000

    Refactor TelemetryHelper to support joined telemetry
    Integrated PanelsTelemetry and JoinedTelemetry to allow telemetry output to both the FtcDashboard and the Driver Station simultaneously

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TelemetryHelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TelemetryHelper.java
@@ -1,23 +1,32 @@
 package org.firstinspires.ftc.teamcode.util;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
 
 public final class TelemetryHelper {
-    private final OpMode opMode;
+    private final Telemetry telemetry;
     private final boolean isEnabled;
 
     public TelemetryHelper(OpMode opMode, boolean isEnabled) {
-        this.opMode = opMode;
         this.isEnabled = isEnabled;
+        Telemetry ftc = opMode.telemetry;
+        Telemetry panels = PanelsTelemetry.INSTANCE.getFtcTelemetry();
+        this.telemetry = new JoinedTelemetry(ftc, panels);
     }
 
     public TelemetryHelper addLine(String text) {
-        if (isEnabled) opMode.telemetry.addLine(text);
+        if (isEnabled) telemetry.addLine(text);
         return this;
     }
 
     public TelemetryHelper addData(String caption, String format, Object... args) {
-        if (isEnabled) opMode.telemetry.addData(caption, format, args);
+        if (isEnabled) telemetry.addData(caption, format, args);
         return this;
+    }
+
+    public void update() {
+        if (isEnabled) telemetry.update();
     }
 }
