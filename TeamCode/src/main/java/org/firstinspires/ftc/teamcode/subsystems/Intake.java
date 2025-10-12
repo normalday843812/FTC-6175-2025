bd1a2d33f8aa3115103a866b89b9aea28d91f0f9
normalday843812 <kaistewart2019@gmail.com>
2025-10-12 01:00:38 -1000

    Refactor telemetry and improve drive logic
    Refactored Intake, Mecanum, and Shooter subsystems to use a single TelemetryHelper instance to avoid repeated object creation.
    
    In Mecanum, corrected the motor power normalization logic to properly scale outputs
    
    In Intake, added independent control for the reverse function, allowing the intake direction to be toggled separately from its running state.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -5,6 +5,7 @@ import static org.firstinspires.ftc.teamcode.config.DriveConfig.TELEMETRY_ENABLE
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.GamepadMap;
+import org.firstinspires.ftc.teamcode.config.HoodServoConfig;
 import org.firstinspires.ftc.teamcode.config.IntakeConfig;
 import org.firstinspires.ftc.teamcode.util.TelemetryHelper;
 
@@ -13,21 +14,22 @@ public class Intake {
     private final DcMotor motor;
     private final GamepadMap map;
     private boolean running=false;
+    private boolean reverse = false;
+    private final TelemetryHelper tele;
 
     public Intake(DcMotor motor, GamepadMap map, OpMode opmode){
         this.motor=motor;
         this.map=map;
         this.opmode=opmode;
+        this.tele = new TelemetryHelper(opmode, HoodServoConfig.TELEMETRY_ENABLED);
     }
-
     public void operate() {
         if (map.intakeToggle) running = !running;
-        double power = 0.0;
-        if (running) power = map.intakeReverseToggle ? IntakeConfig.REVERSE_PWR : IntakeConfig.FORWARD_PWR;
+        if (map.intakeReverseToggle) reverse = !reverse;
+        double power = running ? (reverse ? IntakeConfig.REVERSE_PWR : IntakeConfig.FORWARD_PWR) : 0.0;
         motor.setPower(power);
 
-        new TelemetryHelper(opmode, TELEMETRY_ENABLED)
-                .addLine("--- INTAKE ---")
+       tele.addLine("--- INTAKE ---")
                 .addData("Running:", "%b", running)
                 .addData("Power:", "%.2f", motor.getPower());
     }
