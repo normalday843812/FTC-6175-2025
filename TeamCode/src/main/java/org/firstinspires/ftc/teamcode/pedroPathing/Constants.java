805287f35d0ea4e5f4ca15bf27e55fcaa10d5e7d
normalday843812 <kaistewart2019@gmail.com>
2025-10-12 21:40:26 -1000

    Refactor: Implement voltage compensation in drivetrain
    Introduced voltage compensation to the PedroMecanumDrivetrain to account for battery voltage fluctuations. This is achieved by scaling motor power based on the current voltage supplied by a new DoubleSupplier.
    
    - Updated PedroMecanumDrivetrain to accept a DoubleSupplier for live voltage readings.
    - Refactored Constants to create the drivetrain with a DoubleSupplier that polls all available VoltageSensors.
    - Added a getOpMode() method to RobotHardware to facilitate access to the hardware map for voltage sensors.
    - Enabled voltage compensation by default in the Follower configuration.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import androidx.annotation.NonNull;
+
 import com.pedropathing.Drivetrain;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.firstinspires.ftc.teamcode.localisation.StateEstimator;
 import org.firstinspires.ftc.teamcode.subsystems.Mecanum;
+
+import java.util.function.DoubleSupplier;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
@@ -28,17 +33,34 @@ public class Constants {
     public static Follower createFollower(RobotHardware hw, StateEstimator state) {
         // Ensure motors are initialized before we build the drivetrain
         hw.initDriveMotors();
-        DcMotorEx fl = hw.getFrontLeft();
-        DcMotorEx fr = hw.getFrontRight();
-        DcMotorEx bl = hw.getBackLeft();
-        DcMotorEx br = hw.getBackRight();
-
-        Drivetrain drivetrain = new Mecanum.PedroMecanumDrivetrain(fl, fr, bl, br);
+        Drivetrain drivetrain = getDrivetrain(hw);
 
         // TODO: After running the velocity tuners, set measured max speeds in in/s:
         // follower.setXVelocity(<forward_ips>);
         // follower.setYVelocity(<strafe_ips>);
 
-        return new Follower(followerConstants, state, drivetrain, pathConstraints);
+        Follower follower = new Follower(followerConstants, state, drivetrain, pathConstraints);
+        follower.getDrivetrain().setNominalVoltage(12.0);
+        follower.getDrivetrain().useVoltageCompensation(true);
+        return follower;
+    }
+
+    @NonNull
+    private static Drivetrain getDrivetrain(RobotHardware hw) {
+        DcMotorEx fl = hw.getFrontLeft();
+        DcMotorEx fr = hw.getFrontRight();
+        DcMotorEx bl = hw.getBackLeft();
+        DcMotorEx br = hw.getBackRight();
+
+        DoubleSupplier vsup = () -> {
+            double best = 0.0;
+            for (VoltageSensor vs : hw.getOpMode().hardwareMap.voltageSensor) {
+                double v = vs.getVoltage();
+                if (!Double.isNaN(v) && v > best) best = v;
+            }
+            return best;
+        };
+
+        return new Mecanum.PedroMecanumDrivetrain(fl, fr, bl, br, vsup);
     }
 }
