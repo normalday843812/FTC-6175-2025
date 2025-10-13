87774cacddf7a8b983bfcd1106ce2d0d5065ad0e
normalday843812 <kaistewart2019@gmail.com>
2025-10-12 19:24:32 -1000

    Refactor: PedroPathing and enhance ShotTuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
-import static org.firstinspires.ftc.teamcode.config.DriveConfig.ENCODER_RESOLUTION;
-import static org.firstinspires.ftc.teamcode.config.DriveConfig.FORWARD_ENCODER_DIRECTION;
-import static org.firstinspires.ftc.teamcode.config.DriveConfig.STRAFE_ENCODER_DIRECTION;
+import static org.firstinspires.ftc.teamcode.config.DriveConfig.*;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
@@ -16,37 +14,45 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(5.0); // TODO: put our robot's actual mass
+    public static final FollowerConstants followerConstants =
+            new FollowerConstants()
+                    .mass(5.0); // TODO: put actual robot mass (kg)
 
-    // Can't use RobotHardware because not an OpMode
-    public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1.0)
-            .rightFrontMotorName("front_right_drive")
-            .rightRearMotorName("back_right_drive")
-            .leftRearMotorName("back_left_drive")
-            .leftFrontMotorName("front_left_drive")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD); // TODO: Input actual motor directions
-    // TODO: Add actual tuning constants: https://pedropathing.com/docs/pathing/tuning/automatic
+    public static final MecanumConstants driveConstants =
+            new MecanumConstants()
+                    .maxPower(1.0)
+                    .rightFrontMotorName("front_right_drive")
+                    .rightRearMotorName("back_right_drive")
+                    .leftRearMotorName("back_left_drive")
+                    .leftFrontMotorName("front_left_drive")
+                    .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+                    .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+                    .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+                    .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
 
-    public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-6) // TODO: tune
-            .strafePodX(0.0) // TODO
-            .distanceUnit(DistanceUnit.METER)
-            .hardwareMapName("pinpoint")
-            .encoderResolution(ENCODER_RESOLUTION)
-            .forwardEncoderDirection(FORWARD_ENCODER_DIRECTION) // TODO
-            .strafeEncoderDirection(STRAFE_ENCODER_DIRECTION); // TODO
+    public static final PinpointConstants localizerConstants =
+            new PinpointConstants()
+                    .forwardPodY(PINPOINT_Y_OFFSET_M)
+                    .strafePodX(PINPOINT_X_OFFSET_M)
+                    .distanceUnit(DistanceUnit.METER)
+                    .hardwareMapName("pinpoint")
+                    .encoderResolution(ENCODER_RESOLUTION)
+                    .forwardEncoderDirection(FORWARD_ENCODER_DIRECTION)
+                    .strafeEncoderDirection(STRAFE_ENCODER_DIRECTION);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static final PathConstraints pathConstraints =
+            new PathConstraints(
+                    0.80,
+                    60.0,
+                    1.00,
+                    1.00
+            );
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pinpointLocalizer(localizerConstants)
-                .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+                .pathConstraints(pathConstraints)
                 .build();
     }
 }
