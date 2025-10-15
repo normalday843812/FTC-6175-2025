ab8c9977ac20559514941ff2e0a00095086aadf7
thebestjosh <jzhang27@punahou.edu>
2025-10-14 14:38:01 -1000

    ljll

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -18,13 +18,6 @@ public class Shooter {
     // Hardware
     private final DcMotor shooterMotor;
 
-    // Constants
-    public static double SHOOTER_MOTOR_SPEED = 1.5; // tune this
-
-    // Toggles
-    private boolean prevShooterToggle = false;
-    private boolean runShooter = false;
-
     public Shooter(DcMotor shooterMotor, GamepadMap map, OpMode opmode) {
         this.shooterMotor = shooterMotor;
         this.map = map;
@@ -32,22 +25,13 @@ public class Shooter {
     }
 
     public void OperateShooter() {
-        handleToggles();
-        shooterMotor.setPower(runShooter ? SHOOTER_MOTOR_SPEED : 0.0);
+        shooterMotor.setPower(map.shooterButton);
         if (TELEMETRY_ENABLED) { addTelemetry(); }
     }
 
-    private void handleToggles() {
-        boolean shooterToggle = map.shooterToggle; // define later
-        if (shooterToggle && !prevShooterToggle) {
-            runShooter = !runShooter;
-        }
-        prevShooterToggle = shooterToggle;
-    }
 
     private void addTelemetry() {
         opmode.telemetry.addLine("--- SHOOTER ---");
-        opmode.telemetry.addData("Run Shooter?:", runShooter);
         opmode.telemetry.addData("Shooter Motor Power:", shooterMotor.getPower());
     }
 }
