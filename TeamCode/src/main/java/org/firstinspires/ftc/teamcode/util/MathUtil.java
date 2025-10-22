c496ff3fd1b5c2ff525344cb5dcbdb418716a5bc
normalday843812 <kaistewart2019@gmail.com>
2025-10-22 09:55:08 -1000

    refactor: Remove the unused wrapRad utility method
    The wrapRad function in MathUtil is no longer used and has been removed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtil.java
@@ -6,10 +6,4 @@ public final class MathUtil {
     public static double deadband(double v, double d) {
         return Math.abs(v) > d ? v : 0.0;
     }
-
-    public static double wrapRad(double a) {
-        while (a > Math.PI) a -= 2 * Math.PI;
-        while (a < -Math.PI) a += 2 * Math.PI;
-        return a;
-    }
 }
