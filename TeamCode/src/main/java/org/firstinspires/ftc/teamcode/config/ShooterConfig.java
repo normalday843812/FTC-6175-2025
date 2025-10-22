57d9e0eca04afd39135f87d79918013707b59bc9
normalday843812 <kaistewart2019@gmail.com>
2025-10-22 09:39:47 -1000

    feat: Add manual RPM adjustment for shooter and tune shooter's feedforward term slightly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ShooterConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ShooterConfig.java
@@ -8,11 +8,11 @@ public class ShooterConfig {
     public static boolean TELEMETRY_ENABLED = true;
     public static double TPR_OUTPUT = 384.5397923875;
     public static double TPR_MOTOR = 28.0;
-    public static double TRIGGER_DB = 0.02;
     public static double P = 10.00;
     public static double I = 3.00;
     public static double D = 0.00;
-    public static double F = 0.000555;
+    public static double F = 0.000542;
+    public static double TRIGGER_SCALE = 20;
     public static double MAX_RPM = 5000;
     public static DcMotorSimple.Direction shooterMotorDirection = DcMotorSimple.Direction.FORWARD;
 }
