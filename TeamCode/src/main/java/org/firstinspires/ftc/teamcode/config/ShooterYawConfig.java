cd0ca76ccc2b9d32f4eb049c1d4e8373a1fdb4f6
normalday843812 <kaistewart2019@gmail.com>
2025-10-28 17:41:49 -1000

    feat: integrate ShooterYaw and add limits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ShooterYawConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ShooterYawConfig.java
@@ -7,4 +7,8 @@ public class ShooterYawConfig {
     public static boolean TELEMETRY_ENABLED = true;
     public static double TPR_OUTPUT = ((((1+((double) 46 /17))) * (1+((double) 46 /11))) * 28);
     public static double TPR_MOTOR = 28;
+    public static double YAW_POWER = 0.6;
+    public static int MIN_POSITION = -100;
+    public static int MAX_POSITION = 100;
+    public static double CONTROL_LEVEL = 5;
 }
