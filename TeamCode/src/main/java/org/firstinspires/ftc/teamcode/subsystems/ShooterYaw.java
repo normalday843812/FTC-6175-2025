dae5a364469826c6ac7e1cfffbad44c5d5bc2063
normalday843812 <kaistewart2019@gmail.com>
2025-10-28 17:41:49 -1000

    feat: integrate ShooterYaw and add limits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterYaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterYaw.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.teamcode.config.ShooterYawConfig.CONTROL_LEVEL;
+import static org.firstinspires.ftc.teamcode.config.ShooterYawConfig.MAX_POSITION;
+import static org.firstinspires.ftc.teamcode.config.ShooterYawConfig.MIN_POSITION;
 import static org.firstinspires.ftc.teamcode.config.ShooterYawConfig.TELEMETRY_ENABLED;
+import static org.firstinspires.ftc.teamcode.config.ShooterYawConfig.YAW_POWER;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -12,10 +16,11 @@ import org.firstinspires.ftc.teamcode.util.TelemetryHelper;
 
 public class ShooterYaw {
     private final GamepadMap map;
-    private DcMotorEx shooterYawMotor;
+    private final DcMotorEx shooterYawMotor;
     private final TelemetryHelper tele;
     private SubsystemMode mode = SubsystemMode.MANUAL;
 
+    private int targetPosition = 0;
     private int position = 0;
 
     public ShooterYaw(DcMotorEx shooterYawMotor, GamepadMap map, OpMode opmode) {
@@ -26,22 +31,47 @@ public class ShooterYaw {
 
     public void startTeleop() {
         mode = SubsystemMode.MANUAL;
-        resetShooterYaw();
     }
 
     public void operate() {
         position = shooterYawMotor.getCurrentPosition();
-        shooterYawMotor.setTargetPosition(position + ((int) (map.shooterYaw * 5)));
+
+        if ((map.shooterYaw < 0 && targetPosition > MIN_POSITION) ||
+                (map.shooterYaw > 0 && targetPosition < MAX_POSITION)) {
+
+            targetPosition += (int) (map.shooterYaw * CONTROL_LEVEL);
+        }
+
+        if (map.resetShooterYaw) resetShooterYaw();
+
+        if (targetPosition < MIN_POSITION) targetPosition = MIN_POSITION;
+        if (targetPosition > MAX_POSITION) targetPosition = MAX_POSITION;
+
+        if (shooterYawMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
+            shooterYawMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        shooterYawMotor.setTargetPosition(targetPosition);
+        shooterYawMotor.setPower(YAW_POWER);
+
         addTelemetry();
     }
 
     public void resetShooterYaw() {
         shooterYawMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        targetPosition = 0;
+        position = 0;
+
         shooterYawMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        shooterYawMotor.setTargetPosition(targetPosition);
+        shooterYawMotor.setPower(YAW_POWER);
     }
 
     public void addTelemetry() {
         tele.addLine("=== SHOOTER YAW ===")
-                .addData("Position", "%.0f", (double) position);
+                .addData("Current Pos", "%d", position)
+                .addData("Target Pos", "%d", targetPosition)
+                .addData("Mode", "%s", mode.toString());
     }
 }
