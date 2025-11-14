ab8111f4ed1a854a75dfd899334c9340485567c7
thebestjosh <jzhang27@punahou.edu>
2025-11-13 19:07:51 -1000

    Update TransferConfig.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/TransferConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/TransferConfig.java
@@ -9,10 +9,10 @@ public class TransferConfig {
     public static boolean TELEMETRY_ENABLED = true;
     public static double TRANSFER_1_MIN = 0.38;
     public static double TRANSFER_1_MIN_SHOOTING = 0.24;
-    public static double TRANSFER_1_MAX = 0.1;
+    public static double TRANSFER_1_MAX = 0;
 
-    public static double FLICK_TIME_S = 0.12;
-    public static double RESET_TIME_S = 0.1;
+    public static double FLICK_TIME_S = 0.1;
+    public static double RESET_TIME_S = 0.05;
 
     public static Servo.Direction SERVO_1_DIRECTION = Servo.Direction.FORWARD;
     public static DcMotorSimple.Direction SERVO_2_DIRECTION = DcMotorSimple.Direction.REVERSE;
