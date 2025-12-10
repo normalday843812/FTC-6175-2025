94388ffe44e1533f5d47965f6624843648125207
thebestjosh <jzhang27@punahou.edu>
2025-12-09 15:50:37 -1000

    Update TransferConfig.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/TransferConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/TransferConfig.java
@@ -7,9 +7,9 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Configurable
 public class TransferConfig {
     public static boolean TELEMETRY_ENABLED = true;
-    public static double TRANSFER_1_MIN = 0.36;
-    public static double TRANSFER_1_MIN_SHOOTING = 0.2;
-    public static double TRANSFER_1_MAX = 0;
+    public static double TRANSFER_1_MIN = 0.49;
+    public static double TRANSFER_1_MIN_SHOOTING = 0.31;
+    public static double TRANSFER_1_MAX = 0.2;
 
     public static double FLICK_TIME_S = 2.0;
     public static double RESET_TIME_S = 0.05;
